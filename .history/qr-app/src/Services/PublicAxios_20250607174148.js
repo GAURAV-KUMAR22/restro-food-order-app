4031e28ca9355725760566805728490afa676ad6
GAURAV-KUMAR22 <gorav.panwar99@gmail.com>
2025-06-07 17:57:21 +0530

    fix some line

--- .history/qr-app/src/Services/PublicAxios_20250607174148.js
+++ .history/qr-app/src/Services/PublicAxios_20250607174148.js
@@ -1,4 +1,10 @@
 import axios from "axios";
+const backendUrl =
+  import.meta.env.VITE_MODE === "Production"
+    ? import.meta.env.VITE_BACKEND_PROD
+    : import.meta.env.VITE_BACKEND_DEV;
+
+    
 const publicAxios = axios.create({
   baseURL: `${
     import.meta.env.VITE_MODE === "Production"
